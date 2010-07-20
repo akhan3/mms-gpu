6a1de19403d5d190c7547cbc48ee4fa614e3db87
Aamir Ahmed Khan <aamir@mytum.de>
2010-07-20 00:50:08 -0400

    real based calculations. TODO: debug erraneous potential

--- fmm_calc.cpp
+++ fmm_calc.cpp
@@ -57,27 +57,6 @@ void cmpx_div(  const float a, const float b,
 // =========================================================================
 
 
-// Breadth-first tarversal
-// ===============================
-void traverse_tree_bfs(Box *root, const unsigned int limit) 
-{
-    const unsigned int N = (unsigned int)pow(4, limit);
-    Queue Q(N);
-    Q.enqueue(root);
-    while(!Q.isEmpty()) {
-        // function to perform on node
-        Box *n = (Box*)Q.dequeue();
-        char idstring[100];
-        n->get_idstring(idstring);
-        // printf("this Box is at L%d%s(%d,%d) = L%d(%.1f,%.1f)", n->level, idstring, n->x, n->y, limit, n->cx, n->cy);
-        // NEWLINE;
-        if (n->level < limit) {
-            for(int i=0; i<=3; i++)
-                Q.enqueue(n->child[i]);
-        }
-    }
-}
-
 // Depth-first tarversal
 // ===============================
 void traverse_tree_dfs(Box *n, const unsigned int limit) 
@@ -87,113 +66,155 @@ void traverse_tree_dfs(Box *n, const unsigned int limit)
     n->get_idstring(idstring);
     // printf("this Box is at L%d%s(%d,%d) = L%d(%.1f,%.1f)", n->level, idstring, n->x, n->y, limit, n->cx, n->cy);
     // NEWLINE;
-    if (n->level < limit) {
+    if (n->level < limit)
         for(int i=0; i<=3; i++)
             traverse_tree_dfs(n->child[i], limit);
+}
+
+// Breadth-first tarversal
+// ===============================
+void traverse_tree_bfs(Box *root, const unsigned int limit) 
+{
+    const unsigned int N = (unsigned int)pow(4, limit);
+    Queue Q(N);
+    Q.enqueue(root);
+    while(!Q.isEmpty()) {
+        Box *n = (Box*)Q.dequeue();
+        // function to perform on node
+        char idstring[100];
+        n->get_idstring(idstring);
+        // printf("this Box is at L%d%s(%d,%d) = L%d(%.1f,%.1f)", n->level, idstring, n->x, n->y, limit, n->cx, n->cy);
+        // NEWLINE;
+        // populate queue with children nodes
+        if (n->level < limit)
+            for(int i=0; i<=3; i++)
+                Q.enqueue(n->child[i]);
     }
 }
 
-// to collect potential data
+// Collect potential data BFS
 // ===============================
-void traverse_tree_recurse( Box *n, 
+void collect_potential_bfs( Box *n,
                             const unsigned int limit, 
+                            const unsigned int actual_limit, 
                             const unsigned int H, 
                             float *potential
                           ) 
 {
-    // function to perform on node
-    if (n->level == limit)
-        potential[(int)(n->cy*H+n->cx)] = n->potential;
-    // Recursion
-    if (n->level < limit) {
-        for(int i=0; i<=3; i++)
-            traverse_tree_recurse(n->child[i], limit, H, potential);
+    assert(limit <= actual_limit);
+    printf("Collecting potential data at L%d and smearing it down to L%d...\n", limit, actual_limit);
+    const unsigned int N = (unsigned int)pow(4, limit);
+    Queue Q(N);
+    Q.enqueue(n);
+    while(!Q.isEmpty()) {
+        Box *n = (Box*)Q.dequeue();
+        // function to perform on node
+        if (n->level == limit) {
+            char idstring[100];
+            n->get_idstring(idstring);
+            // smear this potential value to all deeper levels
+            int width = (int)pow(2, actual_limit - limit);
+            // printf("L%d%s(%d,%d)=L%d(%.1f,%.1f) ", n->level, idstring, n->x, n->y, actual_limit, n->cx, n->cy);
+            // printf("width=%d :: ", (int)width);
+            for(unsigned int y = width*n->y; y < width*(n->y+1); y++) {
+                for(unsigned int x = width*n->x; x < width*(n->x+1); x++) {
+                    potential[(int)(y*H+x)] = n->potential;
+                    // printf("(%d,%d)", x,y);
+                }
+            }
+            // NEWLINE;
+        }
+        // populate queue with children nodes        
+        if (n->level < limit)
+            for(int i=0; i<=3; i++)
+                Q.enqueue(n->child[i]);
     }
 }
 
-// to run FMM algo
+// FMM algorithm in BFS
 // ===============================
-void fmm_recurse(   Box *n, 
+void fmm_bfs(   Box *n, 
                     const unsigned int limit, 
+                    const unsigned int actual_limit, 
                     const unsigned int H, 
-                    const unsigned int N,
                     const float *charge, 
                     const unsigned int P    // multipole series (k = 1...P)
                 ) 
 {
-// no FMM steps for Level-0 and Level-1
-    if(n->level >= 2)   // but this condition is not necessary
-    {
-    // function to perform on node
-        float Q = 0;
-        float *a_r = new float[P]();
-        float *a_i = new float[P]();
-        float width = pow(2, limit-n->level);
-        
-        char idstring[100];
-        n->get_idstring(idstring);
-        printf("L%d%s(%d,%d)=L%d(%.1f,%.1f) ", n->level, idstring, n->x, n->y, limit, n->cx, n->cy);
-        // printf("width=%d ", (int)width);
-        // printf("Xrange=[%.0f,%.0f] Yrange=[%.0f,%.0f]", ceil(n->cx-width/2), floor(n->cx+width/2), 
-                                                        // ceil(n->cy-width/2), floor(n->cy+width/2));
-        for(int yy=ceil(n->cy-width/2); yy<=floor(n->cy+width/2); yy++) {
-            for(int xx=ceil(n->cx-width/2); xx<=floor(n->cx+width/2); xx++) {
-                float q = charge[yy*H+xx];
-                if (q != 0) {
-                    Q += q;
-                    float rx_ = xx - n->cx;
-                    float ry_ = yy - n->cy;
-                    printf("{q=%g@(%d,%d)=(%.1f,%.1f)} ", q, xx,yy,rx_,ry_);
-                    for(unsigned int k=1; k<=P; k++) {
-                        float rx__k, ry__k;
-                        cmpx_pow(rx_, ry_, k, &rx__k, &ry__k);
-                        a_r[k-1] += -q * rx__k;
-                        a_i[k-1] += -q * ry__k;
-                        // printf("{a_%d=[%g,%g]} ", k, a_r[k-1],a_i[k-1]);
+    assert(limit <= actual_limit);
+    const unsigned int N = (unsigned int)pow(4, limit);
+    Queue Q(N);
+    Q.enqueue(n);
+    while(!Q.isEmpty()) {
+        Box *n = (Box*)Q.dequeue();
+        // function to perform on node
+        // if(n->level >= 2)   // no FMM steps for Level-0 and Level-1
+                            // but this condition is not necessary
+        {
+            float Q = 0;
+            float *a = new float[P]();
+            float width = pow(2, actual_limit-n->level);
+            
+            char idstring[100];
+            n->get_idstring(idstring);
+            printf("L%d%s(%d,%d)=L%d(%.1f,%.1f) ", n->level, idstring, n->x, n->y, actual_limit, n->cx, n->cy);
+            printf("width=%d ", (int)width);
+            // printf("Xrange=[%.0f,%.0f] Yrange=[%.0f,%.0f]", ceil(n->cx-width/2), floor(n->cx+width/2), 
+                                                            // ceil(n->cy-width/2), floor(n->cy+width/2));
+            for(int yy=ceil(n->cy-width/2); yy<=floor(n->cy+width/2); yy++) {
+                for(int xx=ceil(n->cx-width/2); xx<=floor(n->cx+width/2); xx++) {
+                    float q = charge[yy*H+xx];
+                    if (q != 0) {
+                        Q += q;
+                        float rx_ = xx - n->cx;
+                        float ry_ = yy - n->cy;
+                        // printf("{q=%g@(%d,%d)=[%.1f%s%.1fi]} ", q, xx,yy, rx_, ry_>=0 ? "+" : "", ry_);
+                        // printf("ak = {");
+                        for(unsigned int k=1; k<=P; k++) {
+                            a[k-1] += -q * pow(magnitude(rx_, ry_), k);
+                            // printf("%g, ", a[k-1]);
+                        }
+                        // printf("}");
                     }
                 }
             }
-        }
-        if(Q !=0) {
-            printf("Q=%g ", Q);
-            printf("a_k={");
-            for(unsigned int k=1; k<=P; k++)
-                printf("[%g,%g] ", a_r[k-1],a_i[k-1]);
-            printf("}");
-        }
-
-        float rx;
-        float ry;
-        // printf("interacts with ");
-        for(int i=0; i<27; i++) {
-            Box *ni = n->interaction[i];
-            if (ni == NULL) 
-                break;
-            rx = ni->cx - n->cx;
-            ry = ni->cy - n->cy;
-            printf("[%.0f%s%.0fi] ", rx, ry>=0 ? "+" : "", ry);
-
-            ni->potential += ni->parent->potential;
-            float ln_rx, ln_ry;
-            cmpx_ln(rx,ry, &ln_rx, &ln_ry);
-            ni->potential += Q * ln_rx;
-            // float p_real = 0, p_float = 0;
-            for(unsigned int k=1; k<=P; k++) {
-                float rx_k, ry_k;
-                cmpx_pow(rx, ry, k, &rx_k, &ry_k);
-                float div_r, div_i;
-                cmpx_div(a_r[k-1], a_i[k-1], rx, ry, &div_r, &div_i);
-                ni->potential += div_r / k;
+            if(0 || Q !=0) {
+                printf("Q=%g ", Q);
+                printf("ak={");
+                for(unsigned int k=1; k<=P; k++)
+                    printf("%g, ", a[k-1]);
+                printf("} ");
             }
-            printf("pot = %f ", ni->potential);
+
+            float rx;
+            float ry;
+            // printf("interacts with ");
+            for(int i=0; i<27; i++) {
+                Box *ni = n->interaction[i];
+                if (ni != NULL) 
+                {
+                    float potential_tmp = 0;
+                    rx = ni->cx - n->cx;
+                    ry = ni->cy - n->cy;
+                    // printf("[%.0f%s%.0fi]", rx, ry>=0 ? "+" : "", ry);
+                    potential_tmp += Q * log(magnitude(rx, ry));
+                    for(unsigned int k=1; k<=P; k++) {
+                        potential_tmp += a[k-1] / pow(magnitude(rx, ry), k) / k;
+                    }
+                    potential_tmp += ni->parent->potential;     // inheret potential value from the parent
+                    // printf("%g>", potential_tmp);                    
+                    ni->potential += potential_tmp;
+                    // printf("%g ", ni->potential);
+                }
+            }
+            NEWLINE;
         }
-        NEWLINE;
-    }
-    
-    // Recursion
-    if (n->level < limit)
-        for(int i=0; i<=3; i++)
-            fmm_recurse(n->child[i], limit, H, N, charge, P);
+
+        // populate queue with children nodes
+        if (n->level < limit)
+            for(int i=0; i<=3; i++)
+                Q.enqueue(n->child[i]);
+    } // while(!Q.isEmpty())
 }
 
 
@@ -201,21 +222,21 @@ int fmm_calc(Box *root, const unsigned int limit) {
     const unsigned int N = (unsigned int)pow(4, limit);
     const unsigned int H = (unsigned int)sqrt(N);
     const unsigned int P = 3;   // multipole series truncation (k = 1...P)
-    printf("#################\n");    
-    printf("#  fmm_calc()   #\n");
-    printf("#################\n");    
-    printf("N = %d, log4(N) = %d, sqrt(N) = %d\n", N, limit, H);
+    printf("=================\n");    
+    printf("|  fmm_calc()   |\n");
+    printf("=================\n");    
+    printf("N = %d, log4(N) = %d, sqrt(N) = %d, P = %d\n", N, limit, H, P);
     FILE *fh = NULL;
 
-    printf("#################\n");    
-    printf("#      BFS      #\n");
-    printf("#################\n");    
-    traverse_tree_bfs(root, limit);
-    printf("#################\n");    
-    printf("#      DFS      #\n");
-    printf("#################\n");    
-    traverse_tree_dfs(root, limit);
-    return 0;
+    // printf("#################\n");    
+    // printf("#      BFS      #\n");
+    // printf("#################\n");    
+    // traverse_tree_bfs(root, limit);
+    // printf("#################\n");    
+    // printf("#      DFS      #\n");
+    // printf("#################\n");    
+    // traverse_tree_dfs(root, limit);
+    // return 0;
 
 
 
@@ -223,7 +244,9 @@ int fmm_calc(Box *root, const unsigned int limit) {
     float *charge = new float[N]();
     
 // place a random charge at random location
-    charge[0*H + 1] = 1;
+    int xx = H/2;
+    int yy = H/2;
+    charge[yy*H + xx] = 1;
     // charge[rand_atob(0,H)*H + rand_atob(0,H)] = (float)rand_atob(1, 100);
 // // place a few random charges at random locations
     // for(int yy=0; yy<H; yy++) {
@@ -251,12 +274,12 @@ int fmm_calc(Box *root, const unsigned int limit) {
 
     
 // do the calculations on tree
-    fmm_recurse(root, limit, H, N, charge, P);
+    fmm_bfs(root, limit, limit, H, charge, P);
 
 
 // collect result in a matrix
     float *potential = new float[N]();    // potential matrix
-    traverse_tree_recurse(root, limit, H, potential);
+    collect_potential_bfs(root, limit, limit, H, potential);
 
 // display potential matrix
     printf("potential = [\n");
@@ -274,7 +297,6 @@ int fmm_calc(Box *root, const unsigned int limit) {
     NEWLINE;
 
 
-    
     delete []charge ;
     return EXIT_SUCCESS;
 }
--- visualize_potential.m
+++ visualize_potential.m
@@ -2,7 +2,12 @@ clear
 load charge.dat
 load potential.dat
 
+[ylen, xlen] = size(charge);
+x = 0:xlen-1;
+y = 0:ylen-1;
+[X, Y] = meshgrid(x,y);
+
 subplot(121)
-imagesc(charge); axis image xy;
+imagesc(x,y,charge); axis image xy;
 subplot(122)
-imagesc(potential); axis image xy;
+imagesc(x,y,potential); axis image xy;
