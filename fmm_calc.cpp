f564e57a2176460e0dbbf280e248d1fab6ae783f
Aamir Ahmed Khan <aamir@mytum.de>
2010-07-20 06:51:03 -0400

    FMM calculations correct for line charge. TODO: point charge

--- fmm_calc.cpp
+++ fmm_calc.cpp
@@ -57,6 +57,23 @@ void cmpx_div(  const float a, const float b,
 // =========================================================================
 
 
+// Write matrix to file
+// ===============================
+int matrix2file(const float* matrix, const int rows, const int cols, const char* filename) {
+    FILE *fh = fopen(filename, "w");
+    if(fh == NULL) {
+        printf("FATAL ERROR: Erro opening file %s\n", filename);
+        return EXIT_FAILURE;
+    }
+    for(int r=0; r<rows; r++) {     // axis ij
+        for(int c=0; c<cols; c++)
+            fprintf(fh, "%g ", matrix[r*cols+c]);
+        fprintf(fh, "\n");
+    }
+    fclose(fh);
+    return EXIT_SUCCESS;
+}
+
 // Depth-first tarversal
 // ===============================
 void traverse_tree_dfs(Box *n, const unsigned int limit) 
@@ -142,23 +159,31 @@ void fmm_bfs(   Box *n,
                 ) 
 {
     assert(limit <= actual_limit);
+    printf("Executing FMM algorithm...\n");
+    unsigned int prev_level = 0;
+
     const unsigned int N = (unsigned int)pow(4, limit);
     Queue Q(N);
     Q.enqueue(n);
     while(!Q.isEmpty()) {
         Box *n = (Box*)Q.dequeue();
         // function to perform on node
-        // if(n->level >= 2)   // no FMM steps for Level-0 and Level-1
-                            // but this condition is not necessary
+        if(n->level >= 2)   // no FMM steps for Level-0 and Level-1
         {
+            // inheret with parent's potential
+            n->potential += n->parent->potential;            
             float Q = 0;
             float *a = new float[P]();
             float width = pow(2, actual_limit-n->level);
-            
-            char idstring[100];
-            n->get_idstring(idstring);
-            printf("L%d%s(%d,%d)=L%d(%.1f,%.1f) ", n->level, idstring, n->x, n->y, actual_limit, n->cx, n->cy);
-            printf("width=%d ", (int)width);
+
+            if(prev_level != n->level) {
+                printf("   level%d, %d boxes...\n", n->level, (int)pow(4, n->level));
+                prev_level = n->level;
+            }
+            // char idstring[100];
+            // n->get_idstring(idstring);
+            // printf("L%d%s(%d,%d)=L%d(%.1f,%.1f) ", n->level, idstring, n->x, n->y, actual_limit, n->cx, n->cy);
+            // printf("width=%d ", (int)width);
             // printf("Xrange=[%.0f,%.0f] Yrange=[%.0f,%.0f]", ceil(n->cx-width/2), floor(n->cx+width/2), 
                                                             // ceil(n->cy-width/2), floor(n->cy+width/2));
             for(int yy=ceil(n->cy-width/2); yy<=floor(n->cy+width/2); yy++) {
@@ -178,14 +203,15 @@ void fmm_bfs(   Box *n,
                     }
                 }
             }
-            if(0 || Q !=0) {
-                printf("Q=%g ", Q);
-                printf("ak={");
-                for(unsigned int k=1; k<=P; k++)
-                    printf("%g, ", a[k-1]);
-                printf("} ");
-            }
+            // if(0 || Q !=0) {
+                // printf("Q=%g ", Q);
+                // printf("ak={");
+                // for(unsigned int k=1; k<=P; k++)
+                    // printf("%g, ", a[k-1]);
+                // printf("} ");
+            // }
 
+        // calculation with interaction list 
             float rx;
             float ry;
             // printf("interacts with ");
@@ -196,19 +222,39 @@ void fmm_bfs(   Box *n,
                     float potential_tmp = 0;
                     rx = ni->cx - n->cx;
                     ry = ni->cy - n->cy;
+                    // ni->get_idstring(idstring);
+                    // NEWLINE;
+                    // printf("    ");
+                    // printf("L%d%s: ", ni->level, idstring);
                     // printf("[%.0f%s%.0fi]", rx, ry>=0 ? "+" : "", ry);
                     potential_tmp += Q * log(magnitude(rx, ry));
                     for(unsigned int k=1; k<=P; k++) {
                         potential_tmp += a[k-1] / pow(magnitude(rx, ry), k) / k;
                     }
-                    potential_tmp += ni->parent->potential;     // inheret potential value from the parent
-                    // printf("%g>", potential_tmp);                    
+                    // printf("%.3g + ", potential_tmp);
+                    // printf("%.3g + ", ni->parent->potential);
+                    // printf("%.3g = ", ni->potential);
+                    // inheret potential value from the parent
                     ni->potential += potential_tmp;
-                    // printf("%g ", ni->potential);
+                    // ni->potential += potential_tmp + ni->parent->potential;
+                    // printf("%g", ni->potential);
                 }
             }
-            NEWLINE;
-        }
+            // NEWLINE;
+
+        // calculation with neighbor list at the deepest level
+            if(n->level == limit) {
+                for(int i=0; i<8; i++) {
+                    Box *nb = n->neighbor[i];
+                    if (nb != NULL) 
+                    {
+                        rx = nb->cx - n->cx;
+                        ry = nb->cy - n->cy;
+                        nb->potential += Q * log(magnitude(rx, ry));
+                    }
+                }
+            }
+        }   // if(n->level >= 2)
 
         // populate queue with children nodes
         if (n->level < limit)
@@ -222,11 +268,12 @@ int fmm_calc(Box *root, const unsigned int limit) {
     const unsigned int N = (unsigned int)pow(4, limit);
     const unsigned int H = (unsigned int)sqrt(N);
     const unsigned int P = 3;   // multipole series truncation (k = 1...P)
+    const float charge_probability = 0.001;
     printf("=================\n");    
     printf("|  fmm_calc()   |\n");
     printf("=================\n");    
     printf("N = %d, log4(N) = %d, sqrt(N) = %d, P = %d\n", N, limit, H, P);
-    FILE *fh = NULL;
+    int status = 0;
 
     // printf("#################\n");    
     // printf("#      BFS      #\n");
@@ -243,60 +290,60 @@ int fmm_calc(Box *root, const unsigned int limit) {
 // charge matrix
     float *charge = new float[N]();
     
+
+// Charge configuration
+// ===========================
+// dipole
+    // int l = H/8;
+    // int x0 = H/2;
+    // int y1 = (H-l)/2;
+    // int y2 = (H+l)/2;
+    // charge[y1*H + x0] = 1;
+    // charge[y2*H + x0] = -1;
+
 // place a random charge at random location
     int xx = H/2;
     int yy = H/2;
     charge[yy*H + xx] = 1;
-    // charge[rand_atob(0,H)*H + rand_atob(0,H)] = (float)rand_atob(1, 100);
-// // place a few random charges at random locations
-    // for(int yy=0; yy<H; yy++) {
-        // for(int xx=0; xx<H; xx++) {
-            // if(frand_atob(0, 1) < 0.1)
-                // charge[yy*H + xx] = frand_atob(10.0, 100.0);
+    charge[rand_atob(0,H)*H + rand_atob(0,H)] = 1;
+    charge[rand_atob(0,H)*H + rand_atob(0,H)] = 1;
+    // for(int i=0; i<(int)(N*charge_probability); i++)
+        // charge[rand_atob(0,H)*H + rand_atob(0,H)] = 1;
+        // charge[rand_atob(0,H)*H + rand_atob(0,H)] = frand_atob(1, 10);
+
+// Few random charges at random locations
+    // for(unsigned int yy=0; yy<H; yy++) {
+        // for(unsigned int xx=0; xx<H; xx++) {
+            // if(frand_atob(0, 1) < charge_probability)
+                // charge[yy*H + xx] = 1;
+                // // charge[yy*H + xx] = frand_atob(1, 10);
         // }
     // }
-    
 
-// display charge matrix
-    printf("charge = [\n");
-    fh = fopen("charge.dat", "w");
-    for(unsigned int i=0; i<H; i++) {     // axis ij
-        for(unsigned int j=0; j<H; j++) {
-            printf("%g ", charge[i*H+j]);
-            fprintf(fh, "%g ", charge[i*H+j]);
-        }
-        NEWLINE;
-        fprintf(fh, "\n");
-    }
-    fclose(fh);
-    printf("];\n");
-    NEWLINE;
+// write potential matrix to file
+    status |= matrix2file(charge, H, H, "charge.dat");
 
-    
-// do the calculations on tree
+// Run the FMM algorithm on tree
     fmm_bfs(root, limit, limit, H, charge, P);
-
-
+        
 // collect result in a matrix
     float *potential = new float[N]();    // potential matrix
-    collect_potential_bfs(root, limit, limit, H, potential);
+    collect_potential_bfs(root, limit-0, limit, H, potential);
+// write potential matrix to file
+    status |= matrix2file(potential, H, H, "potential.dat");
 
-// display potential matrix
-    printf("potential = [\n");
-    fh = fopen("potential.dat", "w");
-    for(unsigned int i=0; i<H; i++) {     // axis ij
-        for(unsigned int j=0; j<H; j++) {
-            printf("%g ", potential[i*H+j]);
-            fprintf(fh, "%g ", potential[i*H+j]);
-        }
-        NEWLINE;
-        fprintf(fh, "\n");
+// collect successive stages of FMM in matrices and write them to files
+// for pedagogical purpose only
+    for(unsigned int l=2; l<=limit; l++) {
+        char filename[100];
+        collect_potential_bfs(root, l, limit, H, potential);
+        sprintf(filename, "potential_L%d.dat", l);
+        status |= matrix2file(potential, H, H, filename);
     }
-    fclose(fh);
-    printf("];\n");
-    NEWLINE;
 
 
-    delete []charge ;
-    return EXIT_SUCCESS;
+// closing
+    delete []potential;
+    delete []charge;
+    return status;
 }
--- visualize_potential.m
+++ visualize_potential.m
@@ -1,13 +1,77 @@
-clear
-load charge.dat
-load potential.dat
+% initial loading and setup
+    clear
+    charge = load('charge.dat');
+    potential_fmm = load('potential.dat');
+    [ylen, xlen] = size(charge);
+    x = 0:xlen-1;
+    y = 0:ylen-1;
+    [X, Y] = meshgrid(x,y);
+    H = xlen;
+    N = H^2;
 
-[ylen, xlen] = size(charge);
-x = 0:xlen-1;
-y = 0:ylen-1;
-[X, Y] = meshgrid(x,y);
+% find out the upper and lower limits
+    maxV = max(potential_fmm(:));
+    minV = min(potential_fmm(:));
 
-subplot(121)
-imagesc(x,y,charge); axis image xy;
-subplot(122)
-imagesc(x,y,potential); axis image xy;
+% exact potential matrix
+    potential_exact = zeros(H,H);
+    % charge setup
+    [yy,xx] = find(charge ~= 0);
+    for k = 1:length(yy)
+        q = charge(yy(k), xx(k));
+        X_ = X - (xx(k)-1);
+        Y_ = Y - (yy(k)-1);
+        R_ = sqrt(X_.^2 + Y_.^2);
+        potential_exact = potential_exact + (q * log(R_));
+        potential_exact(find(abs(potential_exact) == inf)) = 0;
+    end
+
+% error calculation
+    relerr = abs(potential_fmm - potential_exact) ./ abs(potential_exact);
+    relerr(isnan(relerr)) = 0;
+    relerr(find(relerr == inf)) = 0;
+    max_error = max(relerr(:))
+    rms_eror = sqrt(mean(relerr(:).^2))
+    
+
+% find out the upper and lower limits
+    maxV = max([potential_fmm(:);  potential_exact(:)]);
+    minV = min([potential_fmm(:);  potential_exact(:)]);
+
+% plotting
+    figure;
+    subplot(221);
+        %imagesc(x,y,charge); axis image xy;
+        spy(flipud(charge));
+        title('Charge distribution');
+    subplot(222);
+        imagesc(x,y,potential_fmm); axis image xy;
+        caxis([minV, maxV]);
+        colorbar;
+        title('Potential from FMM algorithm');
+    subplot(223);
+        imagesc(x,y,potential_exact); axis image xy;
+        caxis([minV, maxV]);
+        colorbar;
+        title('Potential from exact calculation');
+    subplot(224);
+        %imagesc(x,y,relerr); axis image xy; colorbar
+        imagesc(x,y,log10(relerr)); axis image xy; colorbar;
+        title('log_{10} of relative error');
+
+
+% FMM stages animation
+    figure; 
+    imagesc(x,y,charge); axis image xy; colorbar;
+    %spy(flipud(charge));
+    title('Charge distribution');
+    %for l = 2:log(N)/log(4)
+    for l = log(N)/log(4):log(N)/log(4)
+        filename = ['potential_L', num2str(l), '.dat'];
+        potential_fmm = load(filename); 
+        figure; 
+        imagesc(x,y,potential_fmm); 
+        caxis([minV, maxV]);
+        axis image xy;
+        title(['FMM at Level ', num2str(l)]);
+    end
